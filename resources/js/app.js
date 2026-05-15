2fb5e989fbf124984c14bd6d4e835f9eb70dfa71
Zidan Herlangga <zidantech04@gmail.com>
2026-05-16 02:00:23 +0700

    Implement password visibility toggle for user creation, editing, and login forms

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+
+window.togglePassword = function (btn) {
+    const wrapper = btn.closest('.password-toggle');
+    const input = wrapper.querySelector('input');
+    const showIcon = wrapper.querySelector('.password-eye');
+    const hideIcon = wrapper.querySelector('.password-eye-off');
+
+    if (input.type === 'password') {
+        input.type = 'text';
+        showIcon?.classList.add('hidden');
+        hideIcon?.classList.remove('hidden');
+    } else {
+        input.type = 'password';
+        showIcon?.classList.remove('hidden');
+        hideIcon?.classList.add('hidden');
+    }
+};
